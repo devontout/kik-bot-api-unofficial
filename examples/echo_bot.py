bb073a7fdb037546672eb17e95c6ed7ebccac19f
Devon Stout <45133969+devontout@users.noreply.github.com>
2019-06-16 00:23:54 -0700

    Update echo_bot.py

--- examples/echo_bot.py
+++ examples/echo_bot.py
@@ -11,8 +11,8 @@ from kik_unofficial.datatypes.xmpp.roster import FetchRosterResponse, PeerInfoRe
 from kik_unofficial.datatypes.xmpp.sign_up import RegisterResponse, UsernameUniquenessResponse
 from kik_unofficial.datatypes.xmpp.login import LoginResponse, ConnectionFailedResponse
 
-username = 'your_kik_username'
-password = 'your_kik_password'
+username = 'mc_twin'
+password = 'mc_twin'
 
 
 def main():
